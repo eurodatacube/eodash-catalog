fd0742fc1f4ad4d26710728d1944355a25ee7f60
Daniel Santillan <daniel.santillan@eox.at>
2023-07-25 13:09:36 +0200

    chore: moving assets to separate repo (#17)

--- generators/generate_indicators.py
+++ generators/generate_indicators.py
@@ -37,41 +37,42 @@ from pystac.layout import TemplateLayoutStrategy
 def process_catalog_file(file_path):
     print("Processing catalog:", file_path)
     with open(file_path) as f:
-        data = yaml.load(f, Loader=SafeLoader)
+        config = yaml.load(f, Loader=SafeLoader)
         catalog = Catalog(
-            id = data["id"],
-            description = data["description"],
-            title = data["title"],
+            id = config["id"],
+            description = config["description"],
+            title = config["title"],
             catalog_type=CatalogType.RELATIVE_PUBLISHED,
         )
-        for collection in data["collections"]:
-            process_collection_file("../collections/%s.yaml"%(collection), catalog)
+        for collection in config["collections"]:
+            process_collection_file(config, "../collections/%s.yaml"%(collection), catalog)
 
         strategy = TemplateLayoutStrategy(item_template="${collection}/${year}")
-        catalog.normalize_hrefs(data["endpoint"], strategy=strategy)
-        catalog.save(dest_href="../build/%s"%data["id"])
+        catalog.normalize_hrefs(config["endpoint"], strategy=strategy)
+        catalog.save(dest_href="../build/%s"%config["id"])
 
-def process_collection_file(file_path, catalog):
+def process_collection_file(config, file_path, catalog):
     print("Processing collection:", file_path)
     with open(file_path) as f:
         data = yaml.load(f, Loader=SafeLoader)
         for resource in data["Resources"]:
             if "EndPoint" in resource:
                 if resource["Name"] == "Sentinel Hub":
-                    handle_SH_endpoint(resource, data, catalog)
+                    handle_SH_endpoint(config, resource, data, catalog)
                 elif resource["Name"] == "GeoDB":
-                    handle_GeoDB_endpoint(resource, data, catalog)
+                    handle_GeoDB_endpoint(config, resource, data, catalog)
                 elif resource["Name"] == "VEDA":
-                    handle_VEDA_endpoint(resource, data, catalog)
+                    handle_VEDA_endpoint(config, resource, data, catalog)
                 else:
                     raise ValueError("Type of Resource is not supported")
 
-def handle_SH_endpoint(endpoint, data, catalog):
+def handle_SH_endpoint(config, endpoint, data, catalog):
     token = get_SH_token()
     headers = {"Authorization": "Bearer %s"%token}
     endpoint["EndPoint"] = "https://services.sentinel-hub.com/api/v1/catalog/1.0.0/"
     endpoint["CollectionId"] = endpoint["Type"] + "-" + endpoint["CollectionId"] 
     process_STACAPI_Endpoint(
+        config=config,
         endpoint=endpoint,
         data=data,
         catalog=catalog,
@@ -115,7 +116,7 @@ def add_to_catalog(collection, catalog, endpoint, data):
     return link
 
 
-def handle_GeoDB_endpoint(endpoint, data, catalog):
+def handle_GeoDB_endpoint(config, endpoint, data, catalog):
     
     collection = create_collection(endpoint, data, catalog)
     link = add_to_catalog(collection, catalog, endpoint, data)
@@ -155,12 +156,13 @@ def handle_GeoDB_endpoint(endpoint, data, catalog):
         link.extra_fields["country"] = country
         link.extra_fields["city"] = city
         
-    add_collection_metadata(collection, data)
+    add_collection_information(config, collection, data)
     collection.update_extent_from_items()
         
 
-def handle_VEDA_endpoint(endpoint, data, catalog):
+def handle_VEDA_endpoint(config, endpoint, data, catalog):
     process_STACAPI_Endpoint(
+        config=config,
         endpoint=endpoint,
         data=data,
         catalog=catalog,
@@ -229,7 +231,7 @@ def addVisualizationInfo(stac_object:Collection | Item, data, endpoint, file_url
     else:
         print("Visualization endpoint not supported")
 
-def process_STACAPI_Endpoint(endpoint, data, catalog, headers={}):
+def process_STACAPI_Endpoint(config, endpoint, data, catalog, headers={}):
     collection = create_collection(endpoint, data, catalog)
     link = add_to_catalog(collection, catalog, endpoint, data)
     addVisualizationInfo(collection, data, endpoint)
@@ -262,7 +264,7 @@ def process_STACAPI_Endpoint(endpoint, data, catalog, headers={}):
     
     # replace SH identifier with catalog identifier
     collection.id = data["Name"]
-    add_collection_metadata(collection, data)
+    add_collection_information(config, collection, data)
 
     # validate collection after creation
     '''
@@ -272,15 +274,16 @@ def process_STACAPI_Endpoint(endpoint, data, catalog, headers={}):
         print("Issue validation collection: %s"%e)
     '''
 
-def add_collection_metadata(collection, data):
+def add_collection_information(config, collection, data):
     # Add metadata information
     # collection.license = data["License"]
     # TODO: need to review check against SPDX License identifier
+
     if "Story" in data:
         collection.add_asset(
             "story",
             Asset(
-                href="../../assets/%s"%data["Story"],
+                href="%s/%s"%(config["assets_endpoint"], data["Story"]),
                 media_type="text/markdown",
                 roles=["metadata"],
             ),
@@ -289,7 +292,7 @@ def add_collection_metadata(collection, data):
         collection.add_asset(
             "thumbnail",
             Asset(
-                href="../../assets/%s"%data["Image"],
+                href="%s/%s"%(config["assets_endpoint"], data["Image"]),
                 media_type="image/png",
                 roles=["thumbnail"],
             ),
