2f7692a64e82f64ab360494d0f55317c99df0e0f
Lubomir Dolezal <lubomir.dolezal@eox.at>
2024-07-24 11:15:28 +0200

    cache SH token

--- generators/sh_endpoint.py
+++ generators/sh_endpoint.py
@@ -2,19 +2,25 @@ import os
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
+SH_TOKEN_URL = "https://services.sentinel-hub.com/oauth/token"
+_token_cache: dict[str, str] = {}
 
 
-def get_SH_token():
+def get_SH_token() -> str:
     # Your client credentials
-    client_id = os.getenv('SH_CLIENT_ID')
-    client_secret = os.getenv('SH_CLIENT_SECRET')
+    client_id = os.getenv("SH_CLIENT_ID", "")
+    client_secret = os.getenv("SH_CLIENT_SECRET", "")
+    if client_id in _token_cache:
+        return _token_cache[client_id]
     # Create a session
     client = BackendApplicationClient(client_id=client_id)
     oauth = OAuth2Session(client=client)
     # Get token for the session
     token = oauth.fetch_token(
-        token_url='https://services.sentinel-hub.com/oauth/token',
+        token_url=SH_TOKEN_URL,
         client_secret=client_secret,
     )
+    access_token = token["access_token"]
+    _token_cache[client_id] = access_token
 
-    return(token["access_token"])
+    return access_token
